e90179b41100896069a4f834ab29410dfbe849fc
wnsgud0428 <wnsgud0428@naver.com>
2021-12-01 19:08:09 +0900

    feat: 구글 캘린더 차트 만듦

--- src/components/CalendarByJames.js
+++ src/components/CalendarByJames.js
@@ -14,6 +14,9 @@ import axios from "axios"; //api를 통해 값을 받기 위해 사용
 import { propTypes } from "react-bootstrap/esm/Image";
 import { WindowsBalloon } from "node-notifier";
 
+/// google calendar chart를 위해서
+import Chart from "react-google-charts";
+
 const locales = {
   "en-US": require("date-fns/locale/en-US"),
 };
@@ -40,28 +43,38 @@ const events = [
 ];
 
 var squatEvents = [];
+var squatEventsForGoogle = [
+  [
+    { type: "date", id: "Date" },
+    { type: "number", id: "Won/Loss" },
+  ],
+];
+
 function getUserSquat(파라미터) {
   axios
     .get("http://127.0.0.1:8000/apis/users/getuserexercise?username=wnsgu")
     .then((Response) => {
+      console.log(Response.data);
       // squatEvents = [...Response.data]; //deep copy
       for (var i = 0; i < Response.data.length; i++) {
         squatEvents.push({
-          title: Response.data[i].created,
+          title: "스쿼트 " + Response.data[i].squat_count + "회",
           start: Response.data[i].created,
           end: Response.data[i].created,
           exercise_pk: Response.data[i].id,
+          squat_count: Response.data[i].squat_count,
         });
       }
+      for (var i = 0; i < Response.data.length; i++) {
+        //console.log(Response.data[i].created);
+        squatEventsForGoogle.push([
+          new Date(Response.data[i].created),
+          Response.data[i].squat_count,
+        ]);
+      }
+
       console.log("squatEvents:");
       console.log(squatEvents);
-
-      let newDay = {
-        title: Response.data[0].created,
-        allDay: true,
-        start: Response.data[0].created,
-        end: Response.data[0].created,
-      };
 
       // for (var i = 0; i < squatArray.length; i++) {
 
@@ -118,6 +131,8 @@ function CalendarByJames() {
   console.log("squatEventsState:");
   console.log(sqautEventsState);
 
+  var [squatEventsForGoogleState, setSquatEventsForGoogleState] = useState([]);
+
   useEffect(() => {
     getUserSquat(updateSquat);
   }, []);
@@ -128,7 +143,10 @@ function CalendarByJames() {
   }
   function updateSquat() {
     setSqautEventsState(squatEvents);
+    console.log(squatEvents);
+    setSquatEventsForGoogleState(squatEventsForGoogle);
     console.log("업데이트 스쿼트 스테이트");
+    console.log(squatEventsForGoogle[1]);
   }
 
   function onSelectEvent눌렀을때함수(event) {
@@ -138,6 +156,17 @@ function CalendarByJames() {
     // window.location.replace("/result/feed/" + user_exercise_pk);
     window.location.href = "/result/feed/" + user_exercise_pk;
   }
+
+  const eventsData = [
+    [
+      { type: "date", id: "Date" },
+      { type: "number", id: "Won/Loss" },
+    ],
+    [new Date("2021-01-01T13:38:02+09:00"), 1],
+    [new Date("2021-08-25T13:38:02+09:00"), 38024],
+    [new Date("2021-09-25T13:38:02+09:00"), 38024],
+  ];
+  console.log(eventsData);
 
   return (
     <div className="App">
@@ -149,6 +178,17 @@ function CalendarByJames() {
         endAccessor="end"
         style={{ height: 500, margin: "50px" }}
         onSelectEvent={onSelectEvent눌렀을때함수}
+      />
+      <Chart
+        width={1000}
+        height={500}
+        chartType="Calendar"
+        loader={<div>Loading Chart</div>}
+        data={squatEventsForGoogleState}
+        options={{
+          title: "Squat of Year!",
+        }}
+        rootProps={{ "data-testid": "1" }}
       />
     </div>
   );
