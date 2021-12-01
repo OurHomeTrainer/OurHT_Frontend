cef9dc647f0089d91c375c35f6b81259bcdc8540
wnsgud0428 <wnsgud0428@naver.com>
2021-12-01 17:21:23 +0900

    일단 캘린더 완성

--- src/components/CalendarByJames.js
+++ src/components/CalendarByJames.js
@@ -2,7 +2,7 @@ import format from "date-fns/format";
 import getDay from "date-fns/getDay";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 //import DatePicker from "react-datepicker";
@@ -11,6 +11,8 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import jQuery, { data } from "jquery"; //feed에서 들고옴
 
 import axios from "axios"; //api를 통해 값을 받기 위해 사용
+import { propTypes } from "react-bootstrap/esm/Image";
+import { WindowsBalloon } from "node-notifier";
 
 const locales = {
   "en-US": require("date-fns/locale/en-US"),
@@ -38,12 +40,22 @@ const events = [
 ];
 
 var squatEvents = [];
-async function getUserSquat(파라미터) {
+function getUserSquat(파라미터) {
   axios
     .get("http://127.0.0.1:8000/apis/users/getuserexercise?username=wnsgu")
     .then((Response) => {
-      squatEvents = [...Response.data];
-      // var sqautArray = newArray([1, 2, 3]);
+      // squatEvents = [...Response.data]; //deep copy
+      for (var i = 0; i < Response.data.length; i++) {
+        squatEvents.push({
+          title: Response.data[i].created,
+          start: Response.data[i].created,
+          end: Response.data[i].created,
+          exercise_pk: Response.data[i].id,
+        });
+      }
+      console.log("squatEvents:");
+      console.log(squatEvents);
+
       let newDay = {
         title: Response.data[0].created,
         allDay: true,
@@ -103,19 +115,28 @@ function CalendarByJames() {
       end: new Date(2021, 10, 28),
     },
   ]);
+  console.log("squatEventsState:");
+  console.log(sqautEventsState);
 
-  getUserSquat(updateText);
+  useEffect(() => {
+    getUserSquat(updateSquat);
+  }, []);
+  // getUserSquat(updateText);
   function updateText() {
     setText("내일의 밥");
-    console.log(squatEvents);
+    console.log("내일의 밥 -> state변경");
   }
-  function updateSqaut() {
+  function updateSquat() {
     setSqautEventsState(squatEvents);
     console.log("업데이트 스쿼트 스테이트");
   }
 
-  function onSelectEvent눌렀을때함수() {
+  function onSelectEvent눌렀을때함수(event) {
+    console.log(event.title, event.exercise_pk);
+    const user_exercise_pk = event.exercise_pk;
     console.log("눌렀어요!!!");
+    // window.location.replace("/result/feed/" + user_exercise_pk);
+    window.location.href = "/result/feed/" + user_exercise_pk;
   }
 
   return (
